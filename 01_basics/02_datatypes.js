1c9527e09b6533850a9905409d00179d1c6992bb
AryanTyagi1976 <aryantyagy@gmail.com>
2026-06-02 09:04:19 +0000

    datatypes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,22 @@
+"use strict"; // treat all js code as newer version
+// alert(3+3)// we are using nodejs not browser
+
+console.log(3+3); console.log("Aryan") // not recommended as it is not raedable
+
+let my_name = "Aryan"
+console.log(my_name)
+
+let age = 18
+let isLoggedIn = false
+let state;
+
+// number => 2 to power 53
+// bigint
+// string
+// boolean => true/false
+// null => standalnoe value
+// undefined => 
+// symbol => unique
+
+console.log(typeof null) // object
+console.log(typeof undefined) // undefined
